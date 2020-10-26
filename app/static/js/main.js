ed1c78f25f62d94f3cbb15f68c128bf477850e21
David K <kanjurus8@gmail.com>
2020-10-26 16:24:20 -0400

    Navbar complete

---
+++ app/static/js/main.js
@@ -0,0 +1,14 @@
+window.addEventListener('DOMContentLoaded', function (){
+    //Navbar
+const toogle_button = document.getElementsByClassName("toogle-menu")[0]
+const navbar_links = document.getElementsByClassName("navbar-links")[0]
+
+
+toogle_button.addEventListener("click", ()=>{
+    //adds the active class to navbar-link if not exixts and vice versa
+    navbar_links.classList.toggle("active")
+})
+
+
+
+})
