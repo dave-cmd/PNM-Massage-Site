14c82223c5fe60070d9c84173d42f3537b848a84
David K <kanjurus8@gmail.com>
2020-10-31 13:56:36 -0400

    Terms endpoint complete

--- app/routes.py
+++ app/routes.py
@@ -3,3 +3,6 @@ from flask import render_template, request, redirect, url_for
 @app.route("/home")
 def home():
     return render_template("home.html")
+@app.route("/terms-and-conditions")
+def terms():
+    return render_template("terms.html")
